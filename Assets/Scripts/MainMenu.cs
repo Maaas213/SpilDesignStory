45d1733970f6c5a2aca61cc8a0697cb458f35a03
Julie Damm <juliekdamm@gmail.com>
2022-05-11 12:29:09 +0200

    Tilføjelse af animation på døren, når man trykker start. Også tilføjet, at den fader til sort, inden spillet loades.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public Animator doorAnim;
+    public GameObject Canvas;
+    public GameObject Menu;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,15 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
+        StartCoroutine("DoorOpen");
+    }
+
+    IEnumerator DoorOpen()
+    {
+        doorAnim.SetBool("isOpening", true);
+        yield return new WaitForSeconds(1.5f);
+        StartCoroutine(Canvas.GetComponent<FadeToBlack>().FadeBlackOutSquare());
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("Demo Level");
     }
 
