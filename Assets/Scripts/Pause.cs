783158a5d406221d213a2eef47aa041cdcd3a198
Victor <Legendary.nr.23@gmail.com>
2022-05-14 17:57:07 +0200

    Pause ved tryk på "Escape"

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Pause : MonoBehaviour
+{
+
+    public TextMeshProUGUI pauseText;
+
+    public bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && paused == false)
+        {
+            pauseText.gameObject.SetActive(true);
+            paused = true;
+            Time.timeScale = 0.0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && paused == true)
+        {
+            pauseText.gameObject.SetActive(false);
+            paused = false;
+            Time.timeScale = 1.0f;
+        }
+    }
+}
